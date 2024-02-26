0f60b8272b5159b61d5fafc170b2a9ced92f1f2c
Diaz <eddya.diazs@konradlorenz.edu.co>
2024-02-26 16:16:54 -0500

    NuevaPaginaConBoosTrap

---
+++ contenido.js
@@ -0,0 +1,16 @@
+function nuevoParrafo() {
+    var contenido = document.getElementById('contenido');
+    var texto = document.getElementById("texto").value;
+    contenido.innerHTML = contenido.innerHTML + '<p>' + texto + '</p>';
+}
+function imprimir() {
+    var divContents = document.getElementById("contenido").innerHTML;
+    var a = window.open('', '', 'height=500, width=500');
+    a.document.write('<html>');
+    a.mywindow.document.write('<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />');
+    a.document.write('<body > <h1>Resultado</h1> <br>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
